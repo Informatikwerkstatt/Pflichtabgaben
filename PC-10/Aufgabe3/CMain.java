30b270fb2dca31e4f35d8b8e2bd4c5191e0397eb
jinzhaoli2018 <45315137+jinzhaoli2018@users.noreply.github.com>
2018-11-28 00:49:30 +0800

    Update CMain.java

--- PC-10/Aufgabe3/CMain.java
+++ PC-10/Aufgabe3/CMain.java
@@ -21,8 +21,7 @@ public class CMain
         CAuto auto1 = new CSuv("gelb", "benzin", 5, 160, false);
         CAuto auto2 = new CAuto("blau", "VW", 5, 180);
         IFahrzeug fox = auto2;
-        IFahrzeug benzin;
-        benzin = auto1;
+        IFahrzeug benzin = auto1;
         benzin.schalte(5);
         System.out.println(auto1.getGang());
         fox.schalte(3);
